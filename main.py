9064fdf7ece768b2f66b7b94688fb8bc7efc122e
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2022-07-28 00:31:21 +0100

    Added more workouts

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ from win10toast import ToastNotifier
 import time
 
 while True:
+    time.sleep(600)
     workout.squats()
-    time.sleep(10)
+    time.sleep(600)
     workout.pushups()
+    time.sleep(600)
+    workout.walking_lunges()
+    time.sleep(600)
+    workout.plank()
+    time.sleep(600)
+    workout.jumping_jacks()
+    time.sleep(600)
+    workout.pushups_()
+    time.sleep(600)
+    workout.squatss()
+    time.sleep(600)
+    workout.burpees()
+    time.sleep(600)
